fcce8b6fc0fcff56660f21812528eb08ee64c73e
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 18:27:12 +0100

    Add files via upload

---
+++ Practical 2.py
@@ -0,0 +1,9 @@
+character_name = "Tom"
+character_age = "50"
+is_male = False 
+print("There was once a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didnt liked being " + character_age + ", ")
